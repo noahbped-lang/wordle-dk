b8f1bfdda81761e68407bf854ce0a050d93236e3
noahbped-lang <noahbped@gmail.com>
2026-04-29 12:51:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,37 @@ document.addEventListener("keydown", (event) => {
                 document.getElementById("boks3").textContent + 
                 document.getElementById("boks4").textContent + 
                 document.getElementById("boks5").textContent
+
         for (let i = 0; i < 5; i++) {
             if (a[i] === "skabe"[i]) {
                 document.getElementById("boks" + (i + 1)).style.backgroundColor = "green"
-            } else if ("skabe".includes(a[i])) {
-                document.getElementById("boks" + (i + 1)).style.backgroundColor = "yellow"
-            } else {
-                document.getElementById("boks" + (i + 1)).style.backgroundColor = "red"
             }
         }
-        } else if (event.key === "Backspace") {
-    if (boksnummer > 1) {
-        boksnummer = boksnummer - 1
-        document.getElementById("boks" + boksnummer).textContent = ""
-    }
+
+        let brugte = []
+        for (let i = 0; i < 5; i++) {
+            if (a[i] === "skabe"[i]) {
+                brugte.push(a[i])
+            }
+        }
+
+        for (let i = 0; i < 5; i++) {
+            if (a[i] !== "skabe"[i]) {
+                if ("skabe".includes(a[i]) && !brugte.includes(a[i])) {
+                    document.getElementById("boks" + (i + 1)).style.backgroundColor = "yellow"
+                    brugte.push(a[i])
+                } else {
+                    document.getElementById("boks" + (i + 1)).style.backgroundColor = "red"
+                }
+            }
+        }
+        boksnummer = 1
+
+    } else if (event.key === "Backspace") {
+        if (boksnummer > 1) {
+            boksnummer = boksnummer - 1
+            document.getElementById("boks" + boksnummer).textContent = ""
+        }
     } else if (event.key.length === 1) {
         document.getElementById("boks" + boksnummer).textContent = event.key
         boksnummer = boksnummer + 1
